204394376c6e598914b4033403d08a135126eebf
Morten Markussen Lang <lycilph@gmail.com>
2017-07-24 10:58:17 +0200

    The ray tracer can now make a flat black/white image (hardcoded)

--- RayTracer/MainWindow.xaml.cs
+++ RayTracer/MainWindow.xaml.cs
@@ -1,10 +1,57 @@
-﻿namespace RayTracer
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Engine.Geometry;
+using Engine.Shapes;
+
+namespace RayTracer
 {
     public partial class MainWindow
     {
+        private WriteableBitmap bmp;
+        private byte[] pixels;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            bmp = new WriteableBitmap(300, 300, 96, 96, PixelFormats.Rgb24, null);
+            pixels = new byte[bmp.PixelHeight * bmp.PixelWidth * bmp.Format.BitsPerPixel / 8];
+            Image.Source = bmp;
+        }
+
+        private void StartClick(object sender, RoutedEventArgs e)
+        {
+            var sphere = new Sphere(3);
+            var origin = new Point3(0, 0, -20);
+            var view_width = 10;
+            var view_height = 10;
+
+            for (int y = 0; y < 300; y++)
+            {
+                for (int x = 0; x < 300; x++)
+                {
+                    var loc = y * bmp.BackBufferStride + x * 3;
+
+                    var x_pixel = (x - 150) / 300.0 * view_width;
+                    var y_pixel = (y - 150) / 300.0 * view_height;
+                    var z_pixel = 0;
+
+                    var position = new Point3(x_pixel, y_pixel, z_pixel);
+                    var direction = position - origin;
+
+                    var ray = new Ray(origin, direction);
+                    var hit = sphere.Intersect(ray, out double t);
+
+                    if (hit)
+                    {
+                        pixels[loc]     = 255; // Red
+                        pixels[loc + 1] = 255; // Green
+                        pixels[loc + 2] = 255; // Blue
+                    }
+                }
+            }
+            bmp.WritePixels(new Int32Rect(0, 0, bmp.PixelWidth, bmp.PixelHeight), pixels, bmp.PixelWidth * bmp.Format.BitsPerPixel / 8, 0);
         }
     }
 }
--- WriteableBitmapTest/MainWindow.xaml.cs
+++ WriteableBitmapTest/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Reflection;
+using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using Microsoft.Win32;
 
 namespace WriteableBitmapTest
 {
@@ -18,7 +21,7 @@ namespace WriteableBitmapTest
             Image.Source = bmp;
         }
 
-        private void StartClick(object sender, RoutedEventArgs e)
+        private void LinesClick(object sender, RoutedEventArgs e)
         {
             for (int y = 50; y < 250; y += 10)
             {
@@ -32,6 +35,43 @@ namespace WriteableBitmapTest
                 }
             }
             bmp.WritePixels(new Int32Rect(0, 0, bmp.PixelWidth, bmp.PixelHeight), pixels, bmp.PixelWidth * bmp.Format.BitsPerPixel / 8, 0);
+        }
+
+        private void HorizontalClick(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                var loc = i * bmp.BackBufferStride + i * 3;
+
+                pixels[loc] = 255; // Red
+                pixels[loc + 1] = 0; // Green
+                pixels[loc + 2] = 0; // Blue
+            }
+            bmp.WritePixels(new Int32Rect(0, 0, bmp.PixelWidth, bmp.PixelHeight), pixels, bmp.PixelWidth * bmp.Format.BitsPerPixel / 8, 0);
+        }
+
+        private void SaveClick(object sender, RoutedEventArgs e)
+        {
+            var sfd = new SaveFileDialog()
+            {
+                Title = "Please select file to save",
+                InitialDirectory = Assembly.GetExecutingAssembly().Location,
+                DefaultExt = ".png",
+                Filter = "PNG files |*.png"
+            };
+            var result = sfd.ShowDialog();
+            if (result == true)
+            {
+                // Save image here
+                using (var stream = new FileStream(sfd.FileName, FileMode.Create))
+                {
+                    var encoder = new PngBitmapEncoder();
+                    var frame = BitmapFrame.Create(bmp);
+
+                    encoder.Frames.Add(frame);
+                    encoder.Save(stream);
+                }
+            }
         }
     }
 }

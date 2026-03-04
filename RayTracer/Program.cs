7f98d26a1c8e9bae3a29269ec090c8fc264b7e5d
lycilph <lycilph@gmail.com>
2026-03-04 13:35:59 +0100

    Added timing

--- RayTracer/Program.cs
+++ RayTracer/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace RayTracer;
+﻿using System.Diagnostics;
+
+namespace RayTracer;
 
 internal class Program
 {
     static void Main()
     {
+        var sw = Stopwatch.StartNew();
+
         new Renderer().Render("output.ppm");
+
+        sw.Stop();
+        Console.WriteLine("Time elapsed: " + sw.Elapsed);
 
         Console.Write("Press any key to continue...");
         Console.ReadKey();
